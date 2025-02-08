2c52e1ead7521259c68d74e584e24cc9ac9f48b0
Andrey Semenov <semenov.code@gmail.com>
2025-02-08 12:05:19 +0300

    step 1

---
+++ Makefile
@@ -0,0 +1,12 @@
+install:
+	npm ci
+
+publish:
+	npm publish --dry-run
+
+lint:
+	npx eslint .
+
+addPackage:
+	npm link
+			
---
+++ bin/gendiff.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+import { program } from 'commander';
+
+program
+  .name('gendiff')
+  .version('1.0.0')
+  .description('Compares two configuration files and shows a difference.');
+
+program.parse();
+
+if (program.args.length === 0) {
+  program.outputHelp();
+}
