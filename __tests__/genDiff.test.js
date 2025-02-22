bb3dd4a1017b7d5a126327dfeed5456271242229
Andrey Semenov <semenov.code@gmail.com>
2025-02-22 23:56:13 +0300

    refactor tests

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,72 +1,68 @@
+/* eslint-disable object-curly-newline */
+
 import fs from 'fs';
 import path from 'path';
 import genDiff from '../src/index.js';
 
 const getFixturePath = (filename) => path.resolve('__fixtures__', filename);
 
-const diffFlat = fs.readFileSync(getFixturePath('diffFlat.txt'), 'utf-8').trim();
-const diffNested = fs.readFileSync(getFixturePath('diffNested.txt'), 'utf-8').trim();
+const stylishDiff = fs.readFileSync(getFixturePath('diffNested.txt'), 'utf-8').trim();
 const plainDiff = fs.readFileSync(getFixturePath('plainDiff.txt'), 'utf-8').trim();
 const jsonDiff = fs.readFileSync(getFixturePath('jsonDiff.txt'), 'utf-8').trim();
 
-describe('genDiff', () => {
-  describe('should return correct stylish format diff', () => {
-    it('should return correct flat diff with json', () => {
-      const diff = genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'), 'stylish');
-      expect(diff).toEqual(diffFlat);
-    });
-
-    it('should return correct flat diff with yaml', () => {
-      const diff = genDiff(getFixturePath('file1.yaml'), getFixturePath('file2.yml'), 'stylish');
-      expect(diff).toBe(diffFlat);
-    });
-
-    it('should return correct nested diff with json', () => {
-      const diff = genDiff(
-        getFixturePath('file1-nested.json'),
-        getFixturePath('file2-nested.json'),
-        'stylish',
-      );
-      expect(diff).toBe(diffNested);
-    });
-
-    it('should return correct nested diff with yaml', () => {
-      const diff = genDiff(
-        getFixturePath('file1-nested.yaml'),
-        getFixturePath('file2-nested.yaml'),
-        'stylish',
-      );
-      expect(diff).toBe(diffNested);
-    });
-
-    it('should throw error with bad extension', () => {
-      expect(() => genDiff('__fixtures__/test.bad', '__fixtures__/file2.json')).toThrow(
-        'Unsupported file format - bad',
-      );
-    });
-
-    it('should throw error with bad format', () => {
-      expect(() => genDiff('__fixtures__/file1.json', '__fixtures__/file2.json', 'bad')).toThrow(
-        'Unsupported format - bad',
-      );
-    });
+describe.each([
+  {
+    filepath1: 'file1-nested.json',
+    filepath2: 'file2-nested.json',
+    format: 'stylish',
+    extension: 'json',
+    result: stylishDiff,
+  },
+  {
+    filepath1: 'file1-nested.yaml',
+    filepath2: 'file2-nested.yml',
+    format: 'stylish',
+    extension: 'yaml',
+    result: stylishDiff,
+  },
+  {
+    filepath1: 'file1-nested.json',
+    filepath2: 'file2-nested.json',
+    format: 'plain',
+    extension: 'json',
+    result: plainDiff,
+  },
+  {
+    filepath1: 'file1-nested.json',
+    filepath2: 'file2-nested.json',
+    format: 'json',
+    extension: 'json',
+    result: jsonDiff,
+  },
+])('genDiff with $format format', ({ filepath1, filepath2, format, result, extension }) => {
+  it(`should return correct diff with ${extension} files`, () => {
+    const diff = genDiff(getFixturePath(filepath1), getFixturePath(filepath2), format);
+    expect(diff).toBe(result);
   });
+});
 
-  it('should return plain format diff', () => {
-    const diff = genDiff(
-      getFixturePath('file1-nested.json'),
-      getFixturePath('file2-nested.json'),
-      'plain',
+describe.each([
+  {
+    filepath1: 'test.bad',
+    filepath2: 'file2.json',
+    format: 'stylish',
+    error: 'Unsupported file format - bad',
+  },
+  {
+    filepath1: 'file1.json',
+    filepath2: 'file2.json',
+    format: 'bad',
+    error: 'Unsupported format - bad',
+  },
+])('genDiff should throw error', ({ filepath1, filepath2, format, error }) => {
+  it(`${error}`, () => {
+    expect(() => genDiff(getFixturePath(filepath1), getFixturePath(filepath2), format)).toThrow(
+      error,
     );
-    expect(diff).toBe(plainDiff);
-  });
-
-  it('should return json format diff', () => {
-    const diff = genDiff(
-      getFixturePath('file1-nested.json'),
-      getFixturePath('file2-nested.json'),
-      'json',
-    );
-    expect(diff).toBe(jsonDiff);
   });
 });
